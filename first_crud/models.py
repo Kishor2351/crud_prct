6d3d638711247b5d558042d5a95800c916528c4e
kishor <kishorchavan055@gmail.com>
2026-06-26 22:07:50 +0530

    update first phase logic 1

--- first_crud/models.py
+++ first_crud/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+
+class School(models.Model):
+    name = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
+    village = models.CharField(max_length=255)
+
+    class Meta:
+        db_table = 'student_school'
+
+    def __str__(self):
+        return self.name
+
+
+class Student(models.Model):
+    school = models.ForeignKey(School, on_delete=models.CASCADE, related_name='student')
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.first_name
